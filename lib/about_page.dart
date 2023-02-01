9ef215f6bef77e42ea64a39c259cae4c037da6de
Fajar <fajarsidiqm55@gmail.com>
2023-02-01 14:09:34 +0700

    update aboutpage

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -6,15 +6,43 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'About Us',
-          style: TextStyle(color: Colors.white, fontSize: 25),
+        appBar: AppBar(
+          title: Text(
+            'About Us',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
         ),
-      ),
-      body: Column(
-
-      ),
-    );
+        body: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Card(
+                child: (Column(
+              children: [
+                Text("Mozes Hasian Veltin Sinaga"),
+                Text("20103052"),
+              ],
+            ))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Card(
+                child: (Column(
+              children: [
+                Text("Muhammad Albyrra AR"),
+                Text("20103054"),
+              ],
+            ))),
+          ),
+          Padding(
+           padding: const EdgeInsets.only(top: 5),
+            child: Card(
+                child: (Column(
+              children: [
+                Text("Muhammad Fajar Sidiq"),
+                Text("20103055"),
+              ],
+            ))),
+          ),
+        ]));
   }
 }
